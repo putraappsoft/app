a38b245eb37062c25fd94103b69e8ddf8423f1ca
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:22:10 +0700

    update wonder.php

--- film/wonder.php
+++ film/wonder.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2FWonder.2017.[PutraApps].BluRay.360p.mp4?alt=media&token=5eaaa185-4b68-46ac-bf03-c5896b996a03
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2FWonder.2017.[PutraApps].BluRay.360p.mp4?alt=media&token=5eaaa185-4b68-46ac-bf03-c5896b996a03');
+?>
