8af5e7d8524c54a5b7e181493cfe40e4f8d67365
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:26:40 +0700

    update traintobusan.php

--- film/traintobusan.php
+++ film/traintobusan.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/putra-apps-61de7.appspot.com/o/[narashika.cc]%20trntbsn.2016.BluRay.ID.360p.mp4?alt=media&token=20ea25c9-ed77-43cf-a89a-9501f3784d22
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/putra-apps-61de7.appspot.com/o/[narashika.cc]%20trntbsn.2016.BluRay.ID.360p.mp4?alt=media&token=20ea25c9-ed77-43cf-a89a-9501f3784d22');
+?>
