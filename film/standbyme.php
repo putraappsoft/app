9c01242a44636069fe3dbea81286f33f3e86e6cd
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:15:07 +0700

    update standbyme.php

--- film/standbyme.php
+++ film/standbyme.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/videoplayback.mp4?alt=media&token=b420a330-cf88-49c1-a32d-48a23b2f605b
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/videoplayback.mp4?alt=media&token=b420a330-cf88-49c1-a32d-48a23b2f605b');
+?>
