64c6c41ef8ce9575991afdb30009b641dc20bdbc
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 14:08:13 +0700

    update ralphbreakstheinternet.php

--- film/ralphbreakstheinternet.php
+++ film/ralphbreakstheinternet.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2FRalph.Breaks.the.Internet.2018.[KopiFlick].Bluray.360p.mp4?alt=media&token=90a06dc2-161a-4668-befa-aeff3c6ee1c5
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2FRalph.Breaks.the.Internet.2018.[KopiFlick].Bluray.360p.mp4?alt=media&token=90a06dc2-161a-4668-befa-aeff3c6ee1c5');
+?>
