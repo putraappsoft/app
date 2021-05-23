1e0533af95f350054b142e68650ac1ae87635d91
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:56:22 +0700

    update frozen2.php

--- film/frozen2.php
+++ film/frozen2.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/]SOGAFIME.NET]360p.Frozen.II.2019.BluRay.mp4?alt=media&token=402a24cd-28fd-48d8-9b56-82b58e197b8d
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/]SOGAFIME.NET]360p.Frozen.II.2019.BluRay.mp4?alt=media&token=402a24cd-28fd-48d8-9b56-82b58e197b8d');
+?>
