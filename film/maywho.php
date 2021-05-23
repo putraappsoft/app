83026157d248343a7a4694bd689efdac6f44b8be
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:52:09 +0700

    update maywho.php

--- film/maywho.php
+++ film/maywho.php
@@ -1,1 +1,3 @@
-https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/360p.NB21.May.Who.2015.DVDRip.mp4?alt=media&token=0448d6a6-d47d-41b2-916b-d3421f5b18e9
+<?php
+header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/360p.NB21.May.Who.2015.DVDRip.mp4?alt=media&token=0448d6a6-d47d-41b2-916b-d3421f5b18e9');
+?>
