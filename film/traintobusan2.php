a29277950fc0d879199e6ffeb7698cf89b11b869
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-05-23 13:32:19 +0700

    update traintobusan2.php

--- film/traintobusan2.php
+++ film/traintobusan2.php
@@ -1,4 +1,3 @@
 <?php
-header('Location: https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2F81742640ebb53e131c7562634ef2b30b-480p.mp4?alt=media&token=782e36ea-9db3-434a-ae96-cdfe7cf6dd14
-');
+header('Location: https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2F81742640ebb53e131c7562634ef2b30b-480p.mp4?alt=media&token=782e36ea-9db3-434a-ae96-cdfe7cf6dd14');
 ?>
