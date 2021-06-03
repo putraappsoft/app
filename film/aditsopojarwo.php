e6adf40eb762a47e124eb852afed62ecf2cbad13
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-03 18:22:30 +0700

    update aditsopojarwo.php

--- film/aditsopojarwo.php
+++ film/aditsopojarwo.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://lk21official.com/videoplayback?UXNTZEMyRXJGbzJKcDlFelAyaHQ4WjZqS2lMWUdlclZPTnlIaFgzbGtsdnZxOFdHZUZRS2ZlN3ZPWHZhNkVmTmlTOUpvL3J5Mk5FMC9YN1JXTGJZRWs0R3dkY1phQU5Ha0srakhWYm1EVW89OjpnHKXvsWXntOwWqnlC7T13');
+header('Location: https://us.hidester.com/proxy.php?u=eJwBVgCp%2F3M6Nzg6Iju%2Bc6bRhSs701TnAvhBVHTgR1i8ZX6bp5hjesjCYzHaNIUvWioHoOpRFj6%2F2UdgQ20KD4ji1SgT1N%2FBItXKt6cqkQtiD2Tt%2BYpyLejGLCI7nzsnvQ%3D%3D&b=7&f=norefer');
 ?>
