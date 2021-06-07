449458f09a33bf535f7181e2ceaa048c99ccce0a
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-07 09:24:24 +0700

    update mortalkombat.php

--- film/mortalkombat.php
+++ film/mortalkombat.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://firebasestorage.googleapis.com/v0/b/putraservervideo.appspot.com/o/Video%2FDownload%20Film%20Mortal%20Kombat%20(2021)%20Sub%20Indo%20Link%20Google%20Drive.mp4?alt=media&token=6f30fe73-ea69-450e-86ce-c9067db5aa17');
+header('Location: https://pixeldrain.com/api/file/qV4FNxKM');
 ?>
