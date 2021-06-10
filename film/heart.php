e5e11514c69f53a3be5055490b51e54bddfea49d
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-10 08:01:26 +0700

    update heart.php

--- film/heart.php
+++ film/heart.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/8ae3b93eb70f6abd444525bdfc9740d1-360p.mp4?alt=media&token=de1a5f6a-6dfe-4816-bebb-f344e26d49d9');
+header('Location: https://www.googleapis.com/drive/v3/files/1QkGceqkPzJpNbqBdalimvzybmey3XyFP?alt=media&key=AIzaSyD6Sv72543woJFCdr4_DKMnh7cP6olZfGA');
 ?>
