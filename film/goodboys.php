3fdced85b869376a880178f7c34e7676764be8e0
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-10 09:33:31 +0700

    update goodboys.php

--- film/goodboys.php
+++ film/goodboys.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/putraapps.Good.Boys.(2019).WEBRIP.mp4.ID]%20-%20Good%20Boys%20(2019).mp4?alt=media&token=5980b4c1-d230-483b-8034-4aea16feabd3');
+header('Location: https://www.googleapis.com/drive/v3/files/1oZylroc3JO4pWTsasEKVfC1znXTEcfLZ?alt=media&key=AIzaSyD6Sv72543woJFCdr4_DKMnh7cP6olZfGA');
 ?>
