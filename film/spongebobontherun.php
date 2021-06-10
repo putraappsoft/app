b2eabaf6e6ea5b136bac0161769404e8888ab62e
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-10 11:00:28 +0700

    update spongebobontherun.php

--- film/spongebobontherun.php
+++ film/spongebobontherun.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://firebasestorage.googleapis.com/v0/b/metalogsi.appspot.com/o/[480p]The.SpongeBob.Movie.Sponge.on.the.Run.2020.NF.WEB-DL%20[MELONGFILM].mp4?alt=media&token=8ca7007c-94c6-463c-8768-d75cc5c1e87e');
+header('Location: https://www.googleapis.com/drive/v3/files/1QVdgpWJ3PAnR6Tnp6awatUjU39xiqwMl?alt=media&key=AIzaSyD6Sv72543woJFCdr4_DKMnh7cP6olZfGA');
 ?>
