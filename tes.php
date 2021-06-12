cb141e765c5af6444f6ed761396b7dc1fd92999c
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-12 13:06:33 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,6 +1,6 @@
 <?php
 
-$file_id = 'AD6v5dy_l7SvmSV0mdMiUXWy6MKCF4n7Ge8Vduw_dfSap-rhjB9xj2uifrjigl0qwt-kjrDY-StBp5vc0DfeXsgVcgy4D1YqlDb27iV5JQZ3wk0jZLR28-SkO9Q9CRxVPo3sG2kyZ6bb';
+$file_id = 'AD6v5dy';
 $url = 'https://www.blogger.com/video.g?token=' . 
 $file_id;
 $content = file_get_contents( $url );
