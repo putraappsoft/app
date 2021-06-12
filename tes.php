721bb7348f50bfb5ed9a975a9580950f205d2f76
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-12 13:10:07 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,7 +1,7 @@
 <?php
 
-$file_id = 'AD6v5dy';
-$url = 'https://www.blogger.com/video.g?token=' . 
+$file_id = '1M5H1ivjaLIDFkoSskSLM2ysrhJABAwIF';
+$url = 'https://drive.google.com/file/d/' . 
 $file_id;
 $content = file_get_contents( $url );
 
