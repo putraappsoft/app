5d608baccaaaf9a93f464899e5a4d37901114071
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-13 10:04:45 +0700

    update tes.php

--- grabber/example/tes.php
+++ grabber/example/tes.php
@@ -5,10 +5,6 @@ include('../simple_html_dom.php');
 // get DOM from URL or file
 $html = file_get_html('http://www.google.com/');
 
-// replace all input
-foreach($html->find('input') as $e)
-    $e->outertext = '[INPUT]';
-
 // dump contents
 echo $html;
 ?>
