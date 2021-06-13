a0a9927d083d044c81a96e416e7530b51be99a35
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-13 22:04:11 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,3 +1,12 @@
 <?php
 
-echo file_get_html ( 'http://www.google.com/' )-> plaintext ;
+$file_id = '0B9IA6eW3XUC3b05YQnowVG81ems';
+$url = 'https://drive.google.com/get_video_info?docid=' . $file_id;
+$content = file_get_contents( $url );
+
+parse_str( $content, $params );
+$fmt_stream_map = explode( '|', $params['fmt_stream_map'] );
+
+echo '<pre>';
+print_r( $fmt_stream_map );
+echo '</p>';
