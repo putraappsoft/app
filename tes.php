19d1d6f37e3dbaaeb6b2aacabb1916569b0d0304
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-13 09:51:54 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,10 +1,3 @@
 <?php
 
-$file_id = '1M5H1ivjaLIDFkoSskSLM2ysrhJABAwIF';
-$url = 'https://drive.google.com/file/d/' . 
-$file_id;
-$content = file_get_contents( $url );
-
-echo '<pre>';
-print_r( $content );
-echo '</p>';
+echo file_get_html ( 'http://www.google.com/' )-> plaintext ;
