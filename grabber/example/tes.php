b5ad88efac59984b832696662b93c987e2e95a4f
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-13 10:07:48 +0700

    update tes.php

--- grabber/example/tes.php
+++ grabber/example/tes.php
@@ -3,7 +3,7 @@
 include('../simple_html_dom.php');
 
 // get DOM from URL or file
-$html = file_get_html('https://animecrot.xyz/token=AD6v5dwDDfgo5TTOLPjrtoiP0Di0i-bbn4x2tiyv2deFVXh9o4S1lX4bllX7ZD-LbR20XRXj4lOQb7TPGov72iL0-FD2Sa_1Y5uXh037hZInnONhILXK1KPbhcMnBH6A3oiqrcwkMhX0');
+$html = file_get_html('https://www.blogger.com/video.g?token=AD6v5dwDDfgo5TTOLPjrtoiP0Di0i-bbn4x2tiyv2deFVXh9o4S1lX4bllX7ZD-LbR20XRXj4lOQb7TPGov72iL0-FD2Sa_1Y5uXh037hZInnONhILXK1KPbhcMnBH6A3oiqrcwkMhX0');
 
 // dump contents
 echo $html;
