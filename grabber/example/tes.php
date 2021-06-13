ce98089f1165eaf1945f7d86a196cae3bc8d9efd
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-06-13 10:03:56 +0700

    update tes.php

--- grabber/example/tes.php
+++ grabber/example/tes.php
@@ -5,10 +5,6 @@ include('../simple_html_dom.php');
 // get DOM from URL or file
 $html = file_get_html('http://www.google.com/');
 
-// remove all image
-foreach($html->find('img') as $e)
-    $e->outertext = '';
-
 // replace all input
 foreach($html->find('input') as $e)
     $e->outertext = '[INPUT]';
