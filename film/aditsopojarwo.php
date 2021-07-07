6e2030bd2232079c649333cb6d88ed340141ef4e
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-07 08:42:33 +0700

    update aditsopojarwo.php

--- film/aditsopojarwo.php
+++ film/aditsopojarwo.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://www.googleapis.com/drive/v3/files/1-VmD2-uJA2sThBged23c1CmS3Nknq3Nn?alt=media&key=AIzaSyD6Sv72543woJFCdr4_DKMnh7cP6olZfGA');
+header('Location: https://bitball.b-cdn.net/movies/disk3/2021-05/MBAH/Adit.Sopo.Jarwo.The.Movie.2021.WEBDL.1080p.Crots.mp4?download=true&title=Adit%20Sopo%20Jarwo:%20The%20Movie%20(2021)');
 ?>
