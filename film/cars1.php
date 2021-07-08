e1d55e955277c8d72864df77fcb0675fb7d79249
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-08 11:08:00 +0700

    update cars1.php

--- film/cars1.php
+++ film/cars1.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://www.googleapis.com/drive/v3/files/1yXQsZZ-LPK9uMkwHOA5KNouys-G_DYfS?alt=media&key=AIzaSyD6Sv72543woJFCdr4_DKMnh7cP6olZfGA');
+header('Location: https://lh3.googleusercontent.com/NTaQ7QK6M-pvb2TCZBpua6lgVR7AMHfzS5L5X_BsXPDGGlx1GLenSlvc4ZRtQo0ZHWBdRM2KA-np-lQ7Uw=m22');
 ?>
