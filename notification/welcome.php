dd9250d0d542b77bc6a5cd87a1fba534843f8c8f
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-12 20:46:13 +0700

    update welcome.php

--- notification/welcome.php
+++ notification/welcome.php
@@ -1,3 +1,3 @@
 <?php
-header('Location: https://github.com/putraappsoft/putraappsoft.github.io/blob/main/assets/PUTRA%20APPS%20-%20DOWNLOAD%20SEKARANG.mp4?raw=true');
+header('Location: https://support.putraappsoft.eu.org/cdn/delivey/PUTRA%20APPS%20-%20DOWNLOAD%20SEKARANG.mp4');
 ?>
