4544a3ce83282e9e813c9e542dc7b99499f0e309
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-14 18:47:09 +0700

    update tes.php

--- tes.php
+++ tes.php
@@ -1,1 +1,1 @@
-<link rel=“stylesheet” href="<?php echo base_url(‘/key.txt’) ?>" />
+<link rel=“stylesheet” href="<?php echo base_url(‘https://raw.githubusercontent.com/putraappsoft/pupro-video-player/main/skin/videojs-skin-flat.min.css’) ?>" />
