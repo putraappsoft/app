07d002fa55760bee1c94642481ebfbad8793b3ae
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-24 19:11:39 +0700

    update welcome.php

--- notification/welcome.php
+++ notification/welcome.php
@@ -1,4 +1,4 @@
 <?php
-header('Location: https://animecrot.xyz/token=AD6v5dye7IS0e58gCOBkVdytIM0hKkek0Ra-egOuu_gVwdw6cclJhh65-iGUEp9EEdzv7-AhuW5BXfjkK35MuVoobcD02XKCQlVN7My6cEEaYsdAxtxbnE-OhIeV0hUSKQGOdynqCc5E
+header('Location: https://tescokdancok.herokuapp.com/grab.php?url=https://systemnotify27.blogspot.com/2018/07/selamat-datang.html
 ');
 ?>
