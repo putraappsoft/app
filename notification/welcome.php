28b85d6ca49ce45d6b3560098edee5f48c8fba2e
Saputra Luhur Pambudi <75605124+putraappsoft@users.noreply.github.com>
2021-07-25 16:19:41 +0700

    update welcome.php

--- notification/welcome.php
+++ notification/welcome.php
@@ -1,4 +1,4 @@
 <?php
-header('Location: https://milikputraappsoft.herokuapp.com/grab.php?url=https://www.blogger.com/video.g?token=AD6v5dx-dYQz_u-oZoNfG2Avs2v8p6LwcviaK2tMwwgwZ9bTiox3qpBAx4EGKxK3CfB9rcG65vzHZI9wKbMxSMrTqBiKn6uBSxEISzrtT0XzH2YJRVRzK-EvlVkg_7pDbNs1yqFr6tkK
+header('Location: https://milikputraappsoft.herokuapp.com/grab.php?url=https://www.blogger.com/video.g?token=AD6v5dye7IS0e58gCOBkVdytIM0hKkek0Ra-egOuu_gVwdw6cclJhh65-iGUEp9EEdzv7-AhuW5BXfjkK35MuVoobcD02XKCQlVN7My6cEEaYsdAxtxbnE-OhIeV0hUSKQGOdynqCc5E
 ');
 ?>
